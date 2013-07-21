4ef1987833c8c838b0447efaf4b702560d03387b
Flavius Anton <fanton@rosedu.org>
2013-07-21 15:09:17 +0300

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+First::Application.config.secret_token = 'ac6863062258c68b4c1c3a4ff9a03104d061519a3c6be0cbb2adcb25d294d911d005e5b67adc953cd5849e09429b8a4f383d73953f9385478ca9e5240c9e2c21'
